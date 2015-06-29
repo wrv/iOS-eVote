5e8dac94a9a5a67b2ed39f3fac6cd478c0ea7627
willy <willy@iMac-de-Juan-2.local>
2015-06-29 17:09:59 -0500

    Initial Commit

---
+++ iOS-VoteTests/iOS_VoteTests.m
@@ -0,0 +1,40 @@
+//
+//  iOS_VoteTests.m
+//  iOS-VoteTests
+//
+//  Created by willy on 29/06/15.
+//  Copyright (c) 2015 wrv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iOS_VoteTests : XCTestCase
+
+@end
+
+@implementation iOS_VoteTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
